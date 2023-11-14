ab6fa64196c39de6c5246614106fe547d108a303
Aruodore <lucasadomi@gmail.com>
2023-11-15 00:10:35 +0100

    a

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -24,7 +24,7 @@ set_exception_handler("ErrorHandler::handleException");
 header("Content-type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true ");
-// header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 
 
